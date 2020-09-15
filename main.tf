50b7677681c10b662b8c0124f2d387dcaf0b5529
Anthunt <anthunt@users.noreply.github.com>
2020-09-15 19:52:29 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
 
+resource "aws_iam_policy" "policy" {
+
+  for_each = var.policies
+
+  name        = each.key
+  path        = each.value.path
+  description = each.value.description
+
+  policy = each.value.policy
+}
