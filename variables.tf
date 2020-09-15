62caa53602213fb681ea7d6d6353aa81bed1deee
Anthunt <anthunt@users.noreply.github.com>
2020-09-15 19:51:35 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,15 @@
 
+variable "aws" {
+  type = object({
+    region  = string
+    profile = string
+  })
+}
+
+variable "policies" {
+  type = map(object({
+    path        = string
+    description = string
+    policy      = string
+  }))
+}
